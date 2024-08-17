012b8df73fe2d0c035dd8719a0c821785a2a06b5
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-08-16 19:16:29 -0700

    so I dont lose anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './App.css';
+import HelloFromFounder from './components/unique/HelloFromFounder/HelloFromFounder';
 
-// example of a custom hook
 export default function App() {
-  return <main>Hello from template!</main>;
+  return (
+    <>
+      <main className="mx-auto min-h-screen max-w-7xl px-4 max-w-[1200px] py-8">
+        <div>
+          <h1 className="text-center h1">
+            This is a <span>goated</span> component library made for creating
+            social media apps
+          </h1>
+          {/* Insert an extremely casual image of me: Ex: like Dave's smoking pics */}
+          <p className="text-center p">Made for use with shadcn ;)</p>
+          <HelloFromFounder />
+        </div>
+      </main>
+      <footer>
+        <p className="text-center">Made with Love ❤️</p>
+      </footer>
+    </>
+  );
 }
 
